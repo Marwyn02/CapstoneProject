900fda3cd149bf013034683759ede78c2c1eca48
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-08 12:59:31 +0800

    add room reservation page

---
+++ pages/reservation/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RoomForm } from "@/components/form/RoomForm";
+
+const ReservationPage = () => {
+  return (
+    <div className="p-8">
+      <RoomForm />
+    </div>
+  );
+};
+
+export default ReservationPage;
