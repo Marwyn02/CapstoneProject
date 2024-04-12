c2de275baaab2c3f09eb211593f5bd24ed73ec45
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-12 20:43:03 +0800

    add reservation invoice page

---
+++ pages/hotel/reservation/invoice/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoomInvoice from "@/components/form/invoice/RoomInvoice";
+
+const ReservationInvoicePage = () => {
+  return (
+    <div className="p-10">
+      <RoomInvoice />
+    </div>
+  );
+};
+
+export default ReservationInvoicePage;
