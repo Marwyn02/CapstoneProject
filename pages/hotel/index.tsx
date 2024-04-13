407ffc69ea387041eef7cc7fb92d323f216a34c6
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-13 18:20:06 +0800

    add hotel page

---
+++ pages/hotel/index.tsx
@@ -0,0 +1,8 @@
+import { HotelChoiceForm } from "@/components/form/hotel/HotelChoiceForm";
+import React from "react";
+
+const HotelForm = () => {
+  return <HotelChoiceForm />;
+};
+
+export default HotelForm;
