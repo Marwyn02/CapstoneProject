1c9852936be6bd1335c626f5d6d317383d79b373
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-19 20:55:38 +0800

    update hotel index

--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -1,8 +1,24 @@
-import { HotelChoiceForm } from "@/components/form/hotel/HotelChoiceForm";
 import React from "react";
 
+import { HotelChoiceForm } from "@/components/form/hotel/HotelChoiceForm";
+import FormLayout from "@/components/layout/FormLayout";
+import Image from "next/image";
+
 const HotelForm = () => {
-  return <HotelChoiceForm />;
+  return (
+    <FormLayout>
+      {/* <Image
+        src={"/FormHeader.jpg"}
+        alt="Image"
+        height={800}
+        width={800}
+        className="w-full h-[400px]"
+      /> */}
+      <section className="flex justify-center w-full">
+        <HotelChoiceForm />
+      </section>
+    </FormLayout>
+  );
 };
 
 export default HotelForm;
