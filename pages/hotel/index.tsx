bd2d4e41dd057c8c1d1eb5396b536c93a149f44c
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-20 21:35:23 +0800

    update hotel index with the hotel / room information and scroll snap

--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -14,8 +14,30 @@ const HotelForm = () => {
         width={800}
         className="w-full h-[400px]"
       /> */}
-      <section className="flex justify-center w-full">
+      <section className="overflow-y-auto snap-y snap-mandatory max-h-screen">
         <HotelChoiceForm />
+
+        <div className="snap-start h-screen w-screen  flex-nowrap">
+          <section className="flex flex-col">
+            <div className="px-10 py-32 space-y-2">
+              <h3 className="text-2xl font-semibold">Hotel information</h3>
+
+              <p>Address</p>
+              <p>Check-in / Check-out</p>
+              <p>Rooms</p>
+              <p>Pets</p>
+              <p>Internet</p>
+              <p>Transportation</p>
+              <p>Children policy</p>
+              <p>Smoking policy</p>
+              <p>Available services</p>
+
+              <button className="border border-black px-10 py-2.5 font-semibold text-sm">
+                Book
+              </button>
+            </div>
+          </section>
+        </div>
       </section>
     </FormLayout>
   );
