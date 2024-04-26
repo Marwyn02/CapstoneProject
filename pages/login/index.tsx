d910f0bb7aff3374dac24c461b82ccebe79edea7
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-26 23:01:24 +0800

    switch from authentication to login with authentication server side function

---
+++ pages/login/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GetServerSidePropsContext } from "next";
+import { createClient } from "@/utils/supabase/server-props";
+
+import Login from "@/components/authentication/Login";
+
+export default function login() {
+  return <Login />;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const supabase = createClient(context);
+
+  const { data } = await supabase.auth.getUser();
+
+  // console.log(data);
+
+  // if user IS authenticated
+  if (data.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  // if NOT then proceed to login page
+  return {
+    props: {
+      data: null,
+    },
+  };
+}
