4f31ace670c818d7f46f0d0ec6ca55cf2f7d8816
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-30 10:21:40 +0800

    installed parallax landing page

--- components/home/HomeLandingPage.tsx
+++ components/home/HomeLandingPage.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 const HomeLandingPage = () => {
   return (
     <>
-      <div className="bg-[url('/Main-Image.jpg')] relative mx-auto h-screen w-full overflow-y-scroll bg-cover bg-fixed bg-center bg-no-repeat shadow-lg">
-        <div className="absolute px-8 md:px-10 inset-0 flex items-center justify-center">
+      <ParallaxBanner
+        layers={[{ image: "/Main-Image.jpg", speed: -15 }]}
+        className="h-screen w-full"
+      >
+        <div className="absolute h-screen w-full px-8 md:px-10 inset-0 flex items-center justify-center">
           <h1 className="text-slate-100 text-center text-5xl md:text-5xl font-bold">
             Creating Memories, One Stay at a Time.
           </h1>
         </div>
-      </div>
+      </ParallaxBanner>
     </>
   );
 };
