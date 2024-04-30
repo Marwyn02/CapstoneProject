e63ffc28ae2407302dd19b156ad61664c314ec57
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-04-30 10:16:32 +0800

    added home reserve form in the hotel choice page, and update the home reserve for that is usable in every page by state management

--- components/form/hotel/HotelChoiceForm.tsx
+++ components/form/hotel/HotelChoiceForm.tsx
@@ -1,80 +1,110 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Router from "next/router";
 import useStore from "@/store/store";
 
 import Image from "next/image";
-
-const data = [
-  {
-    id: 1,
-    title: "1 Bedroom Villa",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas beatae facere excepturi asperiores, possimus recusandae nam autem libero ipsam voluptates quidem saepe ea unde eius accusamus pariatur doloremque voluptas voluptatem!",
-    image: "/hotel_header.jpg",
-  },
-  {
-    id: 2,
-    title: "2 Bedroom Villa",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas beatae facere excepturi asperiores, possimus recusandae nam autem libero ipsam voluptates quidem saepe ea unde eius accusamus pariatur doloremque voluptas voluptatem!",
-    image: "/hotel-1.jpg",
-  },
-];
+import Link from "next/link";
+import HomeReserveAvailabilityForm from "@/components/home/HomeReserveAvailabilityForm";
 
 export function HotelChoiceForm() {
-  // const { setPlace } = useStore();
+  const { setRoom, setRoomPrice } = useStore();
 
-  // const [placeClicked, setPlaceClicked] = useState<string | null>(null);
+  function choicedRoom(roomName: string, roomPrice: number) {
+    try {
+      setRoom(roomName);
+      setRoomPrice(roomPrice);
 
-  // function onSubmit(values: z.infer<typeof formSchema>) {
-  //   try {
-  //     setPlace(values.place);
-  //     Router.push("/hotel/reservation");
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
+      Router.push("/hotel/reservation");
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
-    <section className="overflow-x-auto snap-x snap-center snap-mandatory flex flex-nowrap">
-      <div className="flex flex-row">
-        {data.map((d) => (
-          <section
-            key={d.id}
-            className="relative flex-none snap-start w-screen h-screen grid grid-cols-3 items-center gap-x-6 py-8"
-          >
-            <p className="absolute text-4xl font-serif text-white z-40 top-28 left-12 border-b pb-5 w-2/5">
-              Our selection of rooms
-            </p>
-            <div className="h-screen z-20 flex justify-end items-end pb-10 bg-gradient-to-r from-black/75 to-transparent px-12 text-slate-200 -mt-8">
-              <div className="space-y-4">
-                <h2 className="text-4xl">{d.title}</h2>
-                <p>{d.description}</p>
-                {/* <button
-                  type="button"
-                  className="border border-black text-sm px-5 py-2"
-                >
-                  Book
-                </button> */}
-              </div>
-            </div>
+    <>
+      <HomeReserveAvailabilityForm />
+      {rooms.map((r) => (
+        <div
+          key={r.id}
+          className="grid grid-cols-5 place-content-center h-screen px-20"
+        >
+          <Image
+            src={r.image}
+            alt="Image"
+            height={200}
+            width={600}
+            className="h-[500px] w-full col-span-3"
+          />
+          <div className="col-span-2 border h-auto">
+            <section className="flex flex-col">
+              <div className="px-10 py-10">
+                <h3 className="text-3xl font-thin text-gray-600 font-serif mb-5">
+                  {r.title}
+                </h3>
 
-            <Image
-              src={d.image}
-              height={1050}
-              width={1050}
-              alt="Image"
-              className="w-screen h-screen absolute brightness-75"
-            />
-          </section>
-        ))}
-      </div>
-    </section>
+                <div className="space-y-1">
+                  <p className="text-xs font-thin font-serif text-gray-600 italic">
+                    Total for a night
+                  </p>
+                  <p className="text-4xl font-medium text-gray-800">
+                    PHP {r.price}
+                  </p>
+                </div>
+
+                <div className="border-y border-gray-400 py-8 mt-8">
+                  <p className="mb-4 font-medium text-xs text-gray-900 uppercase">
+                    Included in this rate
+                  </p>
+                  <p className="indent-4 text-sm font-light text-gray-600">
+                    Breakfast for two each day
+                  </p>
+                </div>
+
+                <div className="bg-gray-200 rounded-md mt-8 px-5 py-8">
+                  <p className="text-gray-600 font-light">
+                    Exclusive benefits —{" "}
+                    <span className="text-sm font-normal">
+                      sign in to your account to activate it.
+                    </span>
+                  </p>
+                </div>
+
+                <div className="mt-5 mb-10">
+                  <p className="text-sm font-light text-gray-600">
+                    Have an account? <Link href={"/register"}>Sign in</Link> or{" "}
+                    <Link href={"/register"}>Create your account</Link>
+                  </p>
+                </div>
+
+                <button
+                  type="button"
+                  className="border border-black px-10 py-2.5 font-semibold text-sm mt-5 hover:bg-black hover:text-white duration-300"
+                  onClick={() => choicedRoom(r.title, r.price)}
+                >
+                  Choose
+                </button>
+              </div>
+            </section>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
-("La Union Staycation Resort");
+const rooms = [
+  {
+    id: 1,
+    image: "/hotel-1.jpg",
+    title: "1 Bedroom Villa",
+    price: 1500,
+  },
 
-("Batangas Beach Resort");
+  {
+    id: 2,
+    image: "/hotel_header.jpg",
+    title: "2 Bedroom Villa",
+    price: 2500,
+  },
+];
