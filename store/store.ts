a1eaf612109c91af21aadeb6886009f2c494e904
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-05-01 22:19:38 +0800

    update store, add total price, changes some properties

--- store/store.ts
+++ store/store.ts
@@ -6,23 +6,25 @@ interface AppState {
     from: any;
     to: any;
   };
-  dayStay: number | undefined;
+  nightStay: number | undefined;
   room: string;
   roomPrice: number;
-  adult: string;
-  children: string;
+  adult: number;
+  children: number;
   childrenAge: string[];
+  total: number;
 }
 
 interface AppActions {
   setPlace: (place: string) => void;
   setDate: (from: any, to: any) => void;
-  setDayStay: (dayStay: number | undefined) => void;
+  setNightStay: (nightStay: number | undefined) => void;
   setRoom: (room: string) => void;
   setRoomPrice: (roomPrice: number) => void;
-  setAdult: (adult: string) => void;
-  setChildren: (children: string) => void;
+  setAdult: (adult: number) => void;
+  setChildren: (children: number) => void;
   setChildrenAge: (childrenAge: string[]) => void;
+  setTotal: (total: number) => void;
 }
 
 const useStore = create<AppState & AppActions>((set) => ({
@@ -31,21 +33,23 @@ const useStore = create<AppState & AppActions>((set) => ({
     from: "",
     to: "",
   },
-  dayStay: 0,
+  nightStay: 0,
   room: "",
   roomPrice: 0,
-  adult: "2 Adults",
-  children: "",
+  adult: 2,
+  children: 0,
   childrenAge: [],
+  total: 0,
 
   setPlace: (place) => set((state) => ({ place })),
   setDate: (from, to) => set((state) => ({ date: { from, to } })),
-  setDayStay: (dayStay) => set((state) => ({ dayStay })),
+  setNightStay: (nightStay) => set((state) => ({ nightStay })),
   setRoom: (room) => set((state) => ({ room })),
   setRoomPrice: (roomPrice) => set((state) => ({ roomPrice })),
   setAdult: (adult) => set((state) => ({ adult })),
   setChildren: (children) => set((state) => ({ children })),
   setChildrenAge: (childrenAge) => set((state) => ({ childrenAge })),
+  setTotal: (total) => set((state) => ({ total })),
 }));
 
 export default useStore;
