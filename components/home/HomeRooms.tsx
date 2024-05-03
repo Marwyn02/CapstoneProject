a9911d58e7f1aef4817c931137a9d0f6bfd69997
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-05-03 19:37:04 +0800

    change carousel button

--- components/home/HomeRooms.tsx
+++ components/home/HomeRooms.tsx
@@ -47,109 +47,17 @@ const HomeRooms = () => {
                 height={1050}
                 width={1050}
                 alt="Image"
-                className="brightness-75 w-full h-[700px] object-cover object-center"
+                className="brightness-75 contrast-100 saturate-150 w-full h-[700px] object-cover object-center"
               />
+              <p className="absolute top-1/2 left-64 text-7xl font-serif font-thin text-white">
+                {r.title}
+              </p>
             </CarouselItem>
           ))}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-      {/* <section className={embla.embla}>
-        <div className="overflow-hidden" ref={emblaRef}>
-          <div className={embla.embla__container}>
-            {rooms.map((r, index) => (
-              <div className={embla.embla__slide} key={index}>
-                <Image
-                  src={r.image}
-                  height={1050}
-                  width={1050}
-                  alt="Image"
-                  className="absolute brightness-75 w-full h-[700px] object-cover object-center"
-                />
-                <p className="absolute bottom-48 text-gray-100 left-32 z-10 text-5xl font-thin font-serif">
-                  {r.title}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="embla__controls">
-          <div className="embla__buttons">
-            <PrevButton
-              onClick={onPrevButtonClick}
-              disabled={prevBtnDisabled}
-            />
-            <NextButton
-              onClick={onNextButtonClick}
-              disabled={nextBtnDisabled}
-            />
-          </div>
-        </div>
-      </section> */}
-      {/* <Carousel className="w-full">
-        <CarouselContent>
-          {rooms.map((r, index) => (
-            <CarouselItem key={index}>
-              <div className="p-1">
-                <section
-                  key={r.id}
-                  className="relative flex-none snap-start w-screen h-screen grid grid-cols-3 items-center gap-x-6 py-8"
-                >
-                  <p className="absolute text-4xl font-serif text-white z-40 top-28 left-12 border-b pb-5 w-2/5">
-                    Our selection of rooms
-                  </p>
-                  <div className="h-screen z-20 flex justify-end items-end pb-10 bg-gradient-to-r from-black/75 to-transparent px-12 text-slate-200 -mt-8">
-                    <div className="space-y-4">
-                      <h2 className="text-4xl">{r.title}</h2>
-                      <p>{r.description}</p>
-                    </div>
-                  </div>
-
-                  <Image
-                    src={r.image}
-                    height={1050}
-                    width={1050}
-                    alt="Image"
-                    className="w-screen h-screen absolute brightness-75"
-                  />
-                </section>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel> */}
-      {/* <section className="overflow-x-auto snap-x snap-center snap-mandatory flex flex-nowrap mt-32">
-        <div className="flex flex-row">
-          {rooms.map((d) => (
-            <section
-              key={d.id}
-              className="relative flex-none snap-start w-screen h-screen grid grid-cols-3 items-center gap-x-6 py-8"
-            >
-              <p className="absolute text-4xl font-serif text-white z-40 top-28 left-12 border-b pb-5 w-2/5">
-                Our selection of rooms
-              </p>
-              <div className="h-screen z-20 flex justify-end items-end pb-10 bg-gradient-to-r from-black/75 to-transparent px-12 text-slate-200 -mt-8">
-                <div className="space-y-4">
-                  <h2 className="text-4xl">{d.title}</h2>
-                  <p>{d.description}</p>
-                </div>
-              </div>
-
-              <Image
-                src={d.image}
-                height={1050}
-                width={1050}
-                alt="Image"
-                className="w-screen h-screen absolute brightness-75"
-              />
-            </section>
-          ))}
-        </div>
-      </section> */}
     </div>
   );
 };
--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -85,10 +85,10 @@ const Carousel = React.forwardRef<
 
     const handleKeyDown = React.useCallback(
       (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === "ArrowLeft") {
+        if (event.key === "ChevronLeft") {
           event.preventDefault();
           scrollPrev();
-        } else if (event.key === "ArrowRight") {
+        } else if (event.key === "ChevronRight") {
           event.preventDefault();
           scrollNext();
         }
@@ -182,7 +182,7 @@ const CarouselItem = React.forwardRef<
       role="group"
       aria-roledescription="slide"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
+        "relative min-w-0 shrink-0 grow-0 basis-full",
         orientation === "horizontal" ? "pl-4" : "pt-4",
         className
       )}
@@ -204,9 +204,9 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute h-10 w-10 bg-transparent border-transparent rounded-full hover:bg-transparent",
         orientation === "horizontal"
-          ? "left-12 top-1/2 translate-y-1/2"
+          ? "left-32 top-1/2 translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -214,7 +214,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ChevronLeft className="h-10 w-10 text-white hover:text-gray-200 duration-300" />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -233,9 +233,9 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "absolute h-10 w-10 bg-transparent border-transparent rounded-full hover:bg-transparent",
         orientation === "horizontal"
-          ? "right-12 top-1/2 translate-y-1/2"
+          ? "right-32 top-1/2 translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -243,7 +243,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
+      <ChevronRight className="h-10 w-10 text-white hover:text-gray-200 duration-300" />
       <span className="sr-only">Next slide</span>
     </Button>
   );
