639145895884372200e99d6bfe75392a96b9f34a
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-05-11 23:58:30 +0800

    Changes: HomeReservation layout and state management inputs

--- components/home/HomeReserveAvailabilityForm.tsx
+++ components/home/HomeReserveAvailabilityForm.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import Router from "next/router";
 
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
+import {
+  Button,
+  GuestAddCountButton,
+  GuestMinusCountButton,
+} from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
   FormField,
@@ -46,6 +50,7 @@ const HomeReserveAvailabilityForm = () => {
     setDate,
     date,
     setNightStay,
+    nightStay,
     setAdult,
     adult,
     setChildren,
@@ -96,10 +101,10 @@ const HomeReserveAvailabilityForm = () => {
       }
 
       if (data || (date.from && date.to)) {
-        setDate(data?.date.from, data?.date.to);
-        setNightStay(daysCheck);
-        setAdult(adultCount);
-        setChildren(childrenCount);
+        // setDate(data?.date.from, data?.date.to);
+        // setNightStay(daysCheck);
+
+        // if (children === 0)
 
         Router.push("/hotel");
         setLoading(false);
@@ -120,12 +125,12 @@ const HomeReserveAvailabilityForm = () => {
 
   // Day Stay Calculator
   useEffect(() => {
-    if (scheduleCheck?.to !== undefined) {
-      const differenceMs = scheduleCheck.to - scheduleCheck.from;
+    if ((date.from && date.to) !== undefined) {
+      const differenceMs = date.to - date.from;
       const daysDifference = Math.ceil(differenceMs / (1000 * 60 * 60 * 24));
-      setDaysCheck(daysDifference);
+      setNightStay(daysDifference);
     }
-  }, [scheduleCheck]);
+  }, [date, setNightStay]);
 
   // Screen size detector
   useEffect(() => {
@@ -164,155 +169,282 @@ const HomeReserveAvailabilityForm = () => {
       <form
         onSubmit={form.handleSubmit(onSubmit)}
         className={
-          path === "/hotel"
-            ? "sticky top-2 flex justify-center items-center gap-x-4 p-2 z-50 bg-white translate-y-14 duration-700"
-            : screenSize.width >= 768
+          screenSize.width >= 768
             ? scrolled
               ? "sticky top-16 z-50 flex justify-center items-center gap-x-4 p-2 bg-white shadow-sm -translate-y-0 duration-700" // scrolled
               : "flex justify-center items-center gap-x-4 p-2 mx-auto bg-white -translate-y-14 duration-700" // not scrolled
-            : "flex-col p-8 bg-white shadow-sm space-y-5" // mobile view
+            : "h-auto grid grid-cols-5 gap-x-0.5 pb-4 px-8 bg-white shadow-sm space-y-5" // mobile view
         }
       >
-        <div className="space-y-2">
-          {/* Date checkin checkout input field  */}
-          <FormField
-            control={form.control}
-            name="date"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[300px] justify-start text-left font-normal",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date.from && date.to ? (
+        {/* Date checking / checkout input field  */}
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="col-span-3">
+              <FormControl>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full md:w-[300px] mt-5 md:mt-0 justify-start text-left text-sm font-normal space-x-2 md:space-x-4",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date.from && date.to ? (
+                        <section className="leading-tight md:flex md:divide-x md:space-x-5">
+                          <div className="flex gap-x-0.5 text-slate-500 font-medium tracking-tightest">
+                            <div className="flex items-center gap-x-0.5">
+                              <p>
+                                {" "}
+                                {new Date(date.from).toLocaleDateString(
+                                  "en-US",
+                                  {
+                                    day: "numeric",
+                                  }
+                                )}
+                              </p>{" "}
+                              <p>
+                                {new Date(date.from).toLocaleDateString(
+                                  "en-US",
+                                  {
+                                    month: "short",
+                                  }
+                                )}
+                              </p>
+                            </div>
+                            <p>-</p>
+                            <div className="flex items-center gap-x-0.5">
+                              <p>
+                                {" "}
+                                {new Date(date.to).toLocaleDateString("en-US", {
+                                  day: "numeric",
+                                })}
+                              </p>{" "}
+                              <p>
+                                {new Date(date.to).toLocaleDateString("en-US", {
+                                  month: "short",
+                                })}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="text-center font-medium md:indent-4">
+                            {nightStay}{" "}
+                            {nightStay && nightStay > 1 ? "nights" : "night"}
+                          </div>
+                        </section>
+                      ) : field.value?.from ? (
+                        field.value.to ? (
                           <>
-                            {new Date(date.from).toLocaleDateString("en-US", {
-                              year: "numeric",
-                              month: "short",
-                              day: "numeric",
-                            }) +
+                            {format(field.value.from, "LLL dd, y") +
                               " " +
                               "-" +
                               " " +
-                              new Date(date.to).toLocaleDateString("en-US", {
-                                year: "numeric",
-                                month: "short",
-                                day: "numeric",
-                              })}
+                              format(field.value.to, "LLL dd, y")}
                           </>
-                        ) : field.value?.from ? (
-                          field.value.to ? (
-                            <>
-                              {format(field.value.from, "LLL dd, y") +
-                                " " +
-                                "-" +
-                                " " +
-                                format(field.value.to, "LLL dd, y")}
-                            </>
-                          ) : (
-                            format(field.value.from, "LLL dd, y")
-                          )
                         ) : (
-                          <span>Arrival - Departure</span>
-                        )}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        initialFocus
-                        mode="range"
-                        defaultMonth={field.value?.from}
-                        selected={field.value}
-                        onSelect={(value) => {
-                          field.onChange(value);
-                          setScheduleCheck(value);
-                        }}
-                        numberOfMonths={2}
-                        disabled={(date) => date < disabledBeforeDate}
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+                          format(field.value.from, "LLL dd, y")
+                        )
+                      ) : (
+                        <span>Arrival - Departure</span>
+                      )}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      initialFocus
+                      mode="range"
+                      defaultMonth={field.value?.from}
+                      selected={field.value}
+                      onSelect={(value) => {
+                        field.onChange(value);
+                        // setScheduleCheck(value);
+                        setDate(value?.from, value?.to);
+                      }}
+                      numberOfMonths={2}
+                      disabled={(date) => date < disabledBeforeDate}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        {/* Mobile Viewed Adult and Children Counting  */}
-        <section className="grid grid-cols-3 items-center md:hidden">
-          <div className="space-y-2">
-            <p className="text-sm font-semibold tracking-wide">Adults</p>
-            {/* Adult Counting */}
-            <div className="flex items-center gap-x-3">
-              <button
-                type="button"
-                className="border border-black px-2.5 py-0.5"
+        {/* Adult and Children Counting Field */}
+        <Popover>
+          <PopoverTrigger className="col-span-2">
+            <Button
+              type="button"
+              className={
+                children > 0
+                  ? "w-full md:w-[150px] flex space-x-2 items-center border border-black font-semibold py-1 px-2"
+                  : "w-full py-2"
+              }
+            >
+              <UserRound className="w-4 mr-2" />
+              <div className="text-start leading-none">
+                <p>
+                  {adult}{" "}
+                  <span className="font-normal text-xs">
+                    {adult > 1 ? "adults" : "adult"}
+                  </span>
+                </p>
+
+                {children > 0 && (
+                  <p>
+                    {children}{" "}
+                    <span className="font-normal text-xs">
+                      {children > 1 ? "children" : "child"}
+                    </span>
+                  </p>
+                )}
+              </div>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[250px] space-y-2">
+            {/* Adult Count Container */}
+            <div className="flex justify-center items-center gap-x-6">
+              <GuestAddCountButton
+                size={"icon"}
                 onClick={() => {
-                  if (adultCount < 20) {
-                    setAdultCount(adultCount + 1);
+                  if (adult < 20) {
+                    setAdult(adult + 1);
                   }
                 }}
-              >
-                +
-              </button>
-              <span>{adultCount}</span>
-              <button
-                type="button"
-                className="border border-black px-2.5 py-0.5"
+              />
+              <div className="flex items-center text-sm font-semibold w-[70px]">
+                <UserRound className="h-4 w-4 mr-1.5 text-gray-600" />
+                {adult}{" "}
+                <span
+                  className={
+                    adult > 1
+                      ? "indent-1 font-normal text-xs"
+                      : "indent-1 mr-1.5 font-normal text-xs"
+                  }
+                >
+                  {adult > 1 ? "adults" : "adult"}
+                </span>
+              </div>
+              <GuestMinusCountButton
+                variant={"outline"}
+                size={"icon"}
                 onClick={() => {
-                  if (adultCount > 0) {
-                    setAdultCount(adultCount - 1);
+                  if (adult > 0) {
+                    setAdult(adult - 1);
                   }
                 }}
-              >
-                -
-              </button>
+              />
             </div>
+
+            {/* Children Count Container  */}
+            <div className="flex justify-center items-center gap-x-6">
+              <GuestAddCountButton
+                size={"icon"}
+                onClick={() => {
+                  if (children < 20) {
+                    setChildren(children + 1);
+                  }
+                }}
+              />
+              <div className="flex justify-center items-center text-sm font-semibold w-[70px]">
+                {children}{" "}
+                <span className="indent-1 font-normal text-xs">
+                  {children > 1 ? "children" : "child"}
+                </span>
+              </div>
+              <GuestMinusCountButton
+                variant={"outline"}
+                size={"icon"}
+                onClick={() => {
+                  if (children > 0) {
+                    setChildren(children - 1);
+                  }
+                }}
+              />
+            </div>
+          </PopoverContent>
+        </Popover>
+
+        {/* <section className="grid grid-cols-2 items-center md:hidden">
+   
+          <div className="flex justify-center items-center gap-x-1">
+            <GuestAddCountButton
+              size={"icon"}
+              onClick={() => {
+                if (adult < 20) {
+                  setAdult(adult + 1);
+                }
+              }}
+            />
+            <div className="flex items-center text-sm font-semibold w-[70px]">
+              <UserRound className="h-4 w-4 mr-1.5 text-gray-600" />
+              {adult > 1 ? (
+                <>
+                  {adult}
+                  <span className="indent-1 font-normal text-xs">adults</span>
+                </>
+              ) : (
+                <>
+                  {adult}
+                  <span className="indent-1 mr-1.5 font-normal text-xs">
+                    adult
+                  </span>
+                </>
+              )}
+            </div>
+            <GuestMinusCountButton
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                if (adult > 0) {
+                  setAdult(adult - 1);
+                }
+              }}
+            />
           </div>
 
-          <div className="space-y-2">
-            <p className="text-sm font-semibold tracking-wide">Children</p>
-            {/* Children Counting */}
-            <div className="flex items-center gap-x-3">
-              <button
-                type="button"
-                className="border border-black px-2.5 py-0.5"
-                onClick={() => {
-                  if (childrenCount < 20) {
-                    setChildrenCount(childrenCount + 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <span>{childrenCount}</span>
-              <button
-                type="button"
-                className="border border-black px-2.5 py-0.5"
-                onClick={() => {
-                  if (childrenCount > 0) {
-                    setChildrenCount(childrenCount - 1);
-                  }
-                }}
-              >
-                -
-              </button>
+          <div className="flex justify-center items-center gap-x-1">
+            <GuestAddCountButton
+              size={"icon"}
+              onClick={() => {
+                if (children < 20) {
+                  setChildren(children + 1);
+                }
+              }}
+            />
+            <div className="flex justify-center items-center text-sm font-semibold w-[70px]">
+              {children > 1 ? (
+                <>
+                  {children}
+                  <span className="indent-1 font-normal text-xs">children</span>
+                </>
+              ) : (
+                <>
+                  {children}
+                  <span className="indent-1 font-normal text-xs">child</span>
+                </>
+              )}
             </div>
+            <GuestMinusCountButton
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                if (children > 0) {
+                  setChildren(children - 1);
+                }
+              }}
+            />
           </div>
+
           <div></div>
-        </section>
+        </section> */}
 
         {/* Desktop view  */}
-        <div className="space-y-2 hidden md:block">
-          {/* Adult Counting */}
+        {/* <div className="space-y-2 hidden md:block">
           <div className="flex items-center gap-x-3">
             <button
               type="button"
@@ -344,7 +476,6 @@ const HomeReserveAvailabilityForm = () => {
         </div>
 
         <div className="space-y-2 hidden md:block">
-          {/* Children Counting */}
           <div className="flex items-center gap-x-3">
             <button
               type="button"
@@ -375,17 +506,17 @@ const HomeReserveAvailabilityForm = () => {
               <Minus className="h-4 w-4" />
             </button>
           </div>
-        </div>
+        </div> */}
 
-        <div className="md:grid md:place-self-center">
-          <Button
-            type="submit"
-            className="w-full md:w-auto mt-2.5 md:mt-0 border border-slate-800 px-8 py-3.5 rounded-none bg-transparent text-xs font-semibold tracking-wide text-[#2A3242] hover:bg-[#2A3242] hover:text-slate-100 duration-300"
-            disabled={loading}
-          >
-            {path !== "/hotel" ? "Book" : "Change book"}
-          </Button>
-        </div>
+        <Button
+          type="submit"
+          variant={"outline"}
+          className="col-span-5 w-full md:w-auto mt-2.5 md:mt-0 px-8 py-3.5 text-xs font-semibold tracking-wide text-[#2A3242] hover:bg-[#2A3242] hover:text-slate-100 duration-300"
+          disabled={loading}
+        >
+          Book
+          {/* {path !== "/hotel" ? "Book" : "Change book"} */}
+        </Button>
       </form>
     </Form>
   );
