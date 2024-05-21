74922c9714366e2ea5c8ec68b23dbb452d0a58d6
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-05-21 23:21:19 +0800

    Changes: Hotel forms, not yet responsive. Add: Hotel Room details

--- components/form/hotel/HotelCard.tsx
+++ components/form/hotel/HotelCard.tsx
@@ -11,7 +11,16 @@ type roomInfoProps = {
   price: number;
 };
 
-const HotelCard = ({ user, info }: { user: User; info: roomInfoProps }) => {
+const HotelCard = ({
+  user,
+  info,
+  onOpenDetails,
+}: {
+  user: User;
+  info: roomInfoProps;
+  openDetails: any;
+  onOpenDetails: any;
+}) => {
   const { setRoom, setRoomPrice } = useStore();
 
   function choicedRoom(roomName: string, roomPrice: number) {
@@ -26,16 +35,13 @@ const HotelCard = ({ user, info }: { user: User; info: roomInfoProps }) => {
   }
   return (
     <>
-      <section className="flex flex-col col-span-2 border md:h-auto px-5 py-4 md:p-10">
-        <h3 className="text-lg md:text-2xl font-semibold text-gray-700 mb-2 md:mb-5">
-          {info.title}
-        </h3>
+      <section className="flex flex-col col-span-4 md:h-auto px-5 py-4 md:py-2 md:px-0">
+        <div className="flex justify-between items-center border-b border-black pb-5">
+          <h3 className="text-lg md:text-2xl font-semibold text-gray-700 mb-2 md:mb-0">
+            {info.title}
+          </h3>
 
-        <div>
-          {/* <p className="text-xs font-thin font-serif text-gray-600 italic">
-            Total for a night
-          </p> */}
-          <p className="text-xl font-semibold md:text-3xl md:font-medium text-gray-800">
+          <p className="text-xl font-semibold md:text-xl md:font-medium text-gray-800">
             PHP {info.price}{" "}
             <span className="text-xs font-thin font-serif text-gray-600 italic">
               / night
@@ -43,18 +49,35 @@ const HotelCard = ({ user, info }: { user: User; info: roomInfoProps }) => {
           </p>
         </div>
 
-        <div className="md:border-y md:border-gray-400 py-4 mt-2 md:py-8 md:mt-8">
-          <p className="mb-4 font-bold underline md:font-medium text-xs text-gray-900 uppercase">
-            Included in this rate
+        <div className="md:border-0 md:border-gray-400 py-4 mt-2 md:pt-5 md:pb-2 md:mt-0">
+          <p className="mb-3 font-bold underline md:font-medium text-xs text-gray-900 uppercase">
+            Highlighted Inclusions
           </p>
-          <p className="indent-4 text-sm font-light text-gray-600">
-            Breakfast for two each day
-          </p>
+          <div className="indent-4 text-sm font-light text-gray-600">
+            <p>Free Wi-Fi</p>
+            <p>Daily Housekeeping</p>
+            <p>Access to Pool</p>
+            <p>Welcome Drink</p>
+          </div>
+        </div>
+
+        <div>
+          <button
+            className="text-xs font-thin underline"
+            onClick={onOpenDetails}
+          >
+            Details and Conditions
+          </button>
         </div>
 
         {user ? (
-          <div className="text-sm bg-gray-200 rounded-md p-4 md:mt-8 md:px-5 md:py-8">
-            <p>Free yogurt hehe</p>
+          <div className="text-sm bg-yellow-50 rounded-md p-4 md:mt-2 md:px-5 md:py-5">
+            <p>Membership promo</p>
+            <div className="indent-4 text-sm font-light text-gray-600">
+              <p>40% Discount on the first day</p>
+              <p>Free breakfast for two each day</p>
+              <p>Welcome amenities</p>
+            </div>
           </div>
         ) : (
           <div className="text-sm bg-gray-200 rounded-md p-4 md:mt-8 md:px-5 md:py-8">
@@ -67,19 +90,34 @@ const HotelCard = ({ user, info }: { user: User; info: roomInfoProps }) => {
           </div>
         )}
 
-        <p className="mt-2 text-xs md:mt-5 md:mb-3 md:text-sm font-light text-gray-600">
-          Have an account? <Link href={"/register"}>Sign in</Link> or{" "}
-          <Link href={"/register"}>Create your account</Link>
-        </p>
+        {!user && (
+          <p className="mt-2 text-xs md:mt-5 md:mb-3 md:text-sm font-light text-gray-600">
+            Have an account? <Link href={"/register"}>Sign in</Link> or{" "}
+            <Link href={"/register"}>Create your account</Link>
+          </p>
+        )}
 
-        <Button
-          type="button"
-          variant={"outline"}
-          className="mt-5"
-          onClick={() => choicedRoom(info.title, info.price)}
-        >
-          Choose
-        </Button>
+        <div className="grid grid-cols-7 items-end">
+          <div className="col-span-2">
+            <p
+              className="text-center font-semibold md:text-sm h-10 px-4 py-2 md:font-medium      whitespace-nowrap    border border-slate-800 bg-slate-800 text-slate-100 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50
+"
+            >
+              PHP {info.price}
+              <span className="text-xs font-thin font-serif text-gray-400 italic">
+                / night
+              </span>
+            </p>
+          </div>
+          <Button
+            type="button"
+            variant={"outline"}
+            className="col-span-5 mt-5 border-slate-800"
+            onClick={() => choicedRoom(info.title, info.price)}
+          >
+            Choose
+          </Button>
+        </div>
       </section>
     </>
   );
