483c9715d9e3bd7211f4ef8f5143067b41f68420
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-05-29 21:01:07 +0800

    Fetch and condition the room for roomSummary

--- pages/hotel/enhancements/reservation/index.tsx
+++ pages/hotel/enhancements/reservation/index.tsx
@@ -5,14 +5,33 @@ import FormLayout from "@/components/layout/FormLayout";
 
 import { createClient } from "@/utils/supabase/server-props";
 import { GetServerSidePropsContext } from "next";
-import { User } from "@supabase/supabase-js";
+import type { User } from "@supabase/supabase-js";
 
-export default function ReservationPage({ user }: { user: User }) {
+type Rooms = {
+  id: string;
+  name: string;
+  image: string;
+  amenities: {
+    amenities: string[];
+  };
+  no_available: number;
+  created_at: string;
+  price: number;
+  no_guest: number;
+};
+
+export default function ReservationPage({
+  user,
+  rooms,
+}: {
+  user: User;
+  rooms: Rooms;
+}) {
   return (
     <FormLayout user={user}>
       <section className="grid grid-cols-2 gap-x-2 md:px-20 lg:px-64">
         <ReservationForm user={user} />
-        <RoomSummary />
+        <RoomSummary rooms={rooms} />
       </section>
     </FormLayout>
   );
@@ -20,13 +39,25 @@ export default function ReservationPage({ user }: { user: User }) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const supabase = createClient(context);
+  const { rm } = context.query;
 
   const {
     data: { user },
-    error,
   } = await supabase.auth.getUser();
 
+  const { data: rooms, error } = await supabase
+    .from("rooms")
+    .select("*")
+    .eq("name", rm);
+
+  const [targetRoom]: any = rooms;
+  const room = { ...targetRoom };
+
+  if (error) {
+    console.error(error, "Error");
+  }
+
   return {
-    props: { user },
+    props: { user, rooms: room },
   };
 }
